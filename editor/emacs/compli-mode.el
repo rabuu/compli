39326c55fd50f44fb6e26189d42a085e54e48088
Rasmus Buurman <rasmus@rbuurman.de>
2025-01-19 16:43:43 +0100

    Add simple Emacs major mode

---
+++ editor/emacs/compli-mode.el
@@ -0,0 +1,16 @@
+(defun compli-types ()
+  '("int" "bool"))
+
+(defun compli-keywords ()
+  '("func" "let" "in" "if" "then" "else" "true" "false"))
+
+(defun compli-font-lock-keywords ()
+  (list
+   `(,(regexp-opt (compli-keywords) 'symbols) . font-lock-keyword-face)
+   `(,(regexp-opt (compli-types) 'symbols) . font-lock-type-face)))
+
+(define-derived-mode compli-mode prog-mode "compli"
+  "Simple major mode for editing compli files."
+  (setq-local font-lock-defaults '(compli-font-lock-keywords)))
+
+(provide 'compli-mode)
