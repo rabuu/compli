042ffecd6bb22bb95cf1a1c355decc80d99e6024
Rasmus Buurman <rasmus@rbuurman.de>
2025-02-28 19:34:19 +0100

    Add examples integration test

---
+++ tests/examples.rs
@@ -0,0 +1,22 @@
+fn run_pipeline(src: &str) {
+    let ast = compli::parse(src).unwrap();
+    let typed_ast = compli::type_check(ast).unwrap();
+    let ir = compli::lower(typed_ast).unwrap();
+    let context = inkwell::context::Context::create();
+    let _module = compli::compile(&context, ir).unwrap();
+}
+
+#[test]
+fn language_tour() {
+    run_pipeline(include_str!("../examples/language-tour.compli"));
+}
+
+#[test]
+fn fib() {
+    run_pipeline(include_str!("../examples/fib.compli"));
+}
+
+#[test]
+fn approx_pi() {
+    run_pipeline(include_str!("../examples/approx-pi.compli"));
+}
