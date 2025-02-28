9753a2ca477cd89fb3d1a562a51d7255f7e1e1ac
Rasmus Buurman <rasmus@rbuurman.de>
2025-02-28 18:35:02 +0100

    lexer: move code around

--- src/parsing/lexer.rs
+++ src/parsing/lexer.rs
@@ -51,6 +51,43 @@ pub enum Token {
     Colon,
 }
 
+impl fmt::Display for Token {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        match self {
+            Token::KwBool => write!(f, "bool"),
+            Token::Bool(b) => write!(f, "{}", b),
+            Token::KwInt => write!(f, "int"),
+            Token::Int(i) => write!(f, "{}", i),
+            Token::KwFloat => write!(f, "float"),
+            Token::Float(x) => write!(f, "{}", x),
+            Token::Ident(id) => write!(f, "{}", id),
+            Token::KwFunc => write!(f, "func"),
+            Token::Assign => write!(f, "="),
+            Token::Equals => write!(f, "=="),
+            Token::Less => write!(f, "<"),
+            Token::LessEq => write!(f, "<="),
+            Token::Greater => write!(f, ">"),
+            Token::GreaterEq => write!(f, ">="),
+            Token::Plus => write!(f, "+"),
+            Token::Minus => write!(f, "-"),
+            Token::Asterisk => write!(f, "*"),
+            Token::Slash => write!(f, "/"),
+            Token::And => write!(f, "&&"),
+            Token::Or => write!(f, "||"),
+            Token::ParenOpen => write!(f, "("),
+            Token::ParenClose => write!(f, ")"),
+            Token::Comma => write!(f, ","),
+            Token::Colon => write!(f, ":"),
+            Token::Bang => write!(f, "!"),
+            Token::KwIf => write!(f, "if"),
+            Token::KwThen => write!(f, "then"),
+            Token::KwElse => write!(f, "else"),
+            Token::KwLet => write!(f, "let"),
+            Token::KwIn => write!(f, "in"),
+        }
+    }
+}
+
 /// Tokenize source code
 pub fn lex() -> impl Parser<char, Vec<(Token, Span)>, Error = ParseErr<char>> {
     let integer = text::int(10).map(Token::Int);
@@ -108,41 +145,4 @@ pub fn lex() -> impl Parser<char, Vec<(Token, Span)>, Error = ParseErr<char>> {
         .padded()
         .repeated()
         .then_ignore(end())
-}
-
-impl fmt::Display for Token {
-    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        match self {
-            Token::KwBool => write!(f, "bool"),
-            Token::Bool(b) => write!(f, "{}", b),
-            Token::KwInt => write!(f, "int"),
-            Token::Int(i) => write!(f, "{}", i),
-            Token::KwFloat => write!(f, "float"),
-            Token::Float(x) => write!(f, "{}", x),
-            Token::Ident(id) => write!(f, "{}", id),
-            Token::KwFunc => write!(f, "func"),
-            Token::Assign => write!(f, "="),
-            Token::Equals => write!(f, "=="),
-            Token::Less => write!(f, "<"),
-            Token::LessEq => write!(f, "<="),
-            Token::Greater => write!(f, ">"),
-            Token::GreaterEq => write!(f, ">="),
-            Token::Plus => write!(f, "+"),
-            Token::Minus => write!(f, "-"),
-            Token::Asterisk => write!(f, "*"),
-            Token::Slash => write!(f, "/"),
-            Token::And => write!(f, "&&"),
-            Token::Or => write!(f, "||"),
-            Token::ParenOpen => write!(f, "("),
-            Token::ParenClose => write!(f, ")"),
-            Token::Comma => write!(f, ","),
-            Token::Colon => write!(f, ":"),
-            Token::Bang => write!(f, "!"),
-            Token::KwIf => write!(f, "if"),
-            Token::KwThen => write!(f, "then"),
-            Token::KwElse => write!(f, "else"),
-            Token::KwLet => write!(f, "let"),
-            Token::KwIn => write!(f, "in"),
-        }
-    }
 }
