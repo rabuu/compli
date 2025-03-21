cad1db1673c0ac871509dea9b2fd0c0c2f4700da
Rasmus Buurman <rasmus@rbuurman.de>
2025-03-21 19:02:41 +0100

    emacs: Fix comment highlighting & add (doc)comments

--- editor/emacs/compli-mode.el
+++ editor/emacs/compli-mode.el
@@ -1,21 +1,27 @@
-(defvar compli-mode-syntax-table
-  (let ((table (make-syntax-table)))
-    (modify-syntax-entry ?/ "< 12" table)
-    (modify-syntax-entry ?\n ">" table)
-    table))
+;;; compli-mode.el --- Simple major mode for editing compli files  -*- lexical-binding: t; -*-
 
-(defvar compli-tab-width 4 "Width of a tab for compli-mode")
+;;; Commentary:
+;; To use this major mode manually:
+;; M-x eval-buffer, then go to a compli file and M-x compli-mode
+
+;;; Code:
+
+(defvar compli-tab-width 4 "Width of a tab for 'compli-mode'.")
 
 (defun compli-types ()
+  "Data types in the compli language."
   '("int" "bool" "float"))
 
 (defun compli-keywords ()
+  "Keywords in the compli language."
   '("def" "rec" "let" "in" "if" "then" "else" "true" "false"))
 
 (defun compli-operators ()
+  "Builtin functions in the compli language."
   '("trace" "cast_int" "cast_float" "sqrt" "input_int" "input_float"))
 
 (defun compli-font-lock-defaults ()
+  "Font lock defaults for the compli language."
   (list
    `(,(regexp-opt (compli-keywords) 'symbols) . font-lock-keyword-face)
    `(,(regexp-opt (compli-operators) 'symbols) . font-lock-builtin-face)
@@ -34,7 +40,8 @@
   (setq comment-start "//")
   (setq comment-end "")
 
-  (modify-syntax-entry ?/ "< 12" compli-mode-syntax-table)
-  (modify-syntax-entry ?\n ">" compli-mode-syntax-table))
+  (modify-syntax-entry ?/ ". 12b" compli-mode-syntax-table)
+  (modify-syntax-entry ?\n "> b" compli-mode-syntax-table))
 
 (provide 'compli-mode)
+;;; compli-mode.el ends here
